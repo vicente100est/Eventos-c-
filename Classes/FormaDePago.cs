0046980f73145f699914c4f3435446f8f87e598b
Vicente Estrada <67867277+vicente100est@users.noreply.github.com>
2022-11-16 18:15:34 -0600

    EVENTS C#

---
+++ Classes/FormaDePago.cs
@@ -0,0 +1,45 @@
+﻿namespace Classes
+{
+    public enum TipoAlerta
+    {
+        Error = 1,
+        Advertencia = 2,
+        Exito = 3
+    }
+
+    public enum TipoPago
+    {
+        Tarjeta = 1,
+        Transferencia = 2,
+        Otros = 3
+    }
+
+    public delegate void CambioFormaHandler(TipoPago tipoPago, TipoAlerta tipoAlerta);
+
+    public class FormaDePago
+    {
+        private TipoPago _tipo;
+        public event CambioFormaHandler CambioFormaPago;
+        public TipoPago Tipo
+        {
+            get
+            {
+                return _tipo;
+            }
+            set
+            {
+                TipoAlerta tipoAlerta = TipoAlerta.Error;
+
+                if (value.Equals(TipoPago.Tarjeta)
+                    || value.Equals(TipoPago.Transferencia)
+                    || value.Equals(TipoPago.Otros))
+                {
+                    _tipo = value;
+                    tipoAlerta = TipoAlerta.Exito;
+                }
+
+                CambioFormaPago(Tipo, tipoAlerta);
+            }
+        }
+    }
+}
---
+++ TimerEventExample/Program.cs
@@ -0,0 +1,27 @@
+﻿using Classes;
+
+public class Program
+{
+    private static void Main(string[] args)
+    {
+        string tipoPago;
+
+        Console.WriteLine("Ingresa una forma de pago: ");
+        Console.WriteLine("1.- Tarjeta        2.- Transferencia\n3.- Otros");
+        tipoPago = Console.ReadLine();
+
+        FormaDePago fm = new FormaDePago();
+
+        fm.CambioFormaPago += fm_SeleccioneFormaPago;
+
+        fm.Tipo = (TipoPago)Enum.Parse(typeof(TipoPago), tipoPago);
+    }
+
+    public static void fm_SeleccioneFormaPago(TipoPago tipo, TipoAlerta tipoAlerta)
+    {
+        if(tipoAlerta.Equals(TipoAlerta.Error))
+            Console.WriteLine("Error, elegiste forma de pago erronea");
+        else if(tipoAlerta.Equals(TipoAlerta.Error))
+            Console.WriteLine("Forma de pago seleccionada: {0} ", tipo.ToString);
+    }
+}
